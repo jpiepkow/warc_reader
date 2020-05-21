e36ef09652f40d3f2e4032bde8c63d700bf07b24
jordan piepkow <jordanpiepkow@jordans-MacBook-Pro.local>
2020-05-21 14:18:14 -0400

    working v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use rust_warc::{WarcError, WarcReader, WarcRecord};
+use std::collections::HashMap;
+
 pub enum Warc {
     Wait,
     WarcResult(WarcRecord),
@@ -7,29 +9,29 @@ pub struct WarcParser {
     pub records: Vec<WarcRecord>,
     pub response: reqwest::Response,
     pub buffers: Vec<u8>,
-    pub can_poll: bool
+    pub can_poll: bool,
+    pub record_ids: HashMap<String, bool>,
 }
 
-    
 impl WarcParser {
     pub async fn new(url: String) -> Result<WarcParser, Box<dyn std::error::Error>> {
         Ok(WarcParser {
             buffers: [].to_vec(),
             records: vec![],
             response: reqwest::get(&url).await?,
-            can_poll: true
+            can_poll: true,
+            record_ids: HashMap::new(),
         })
     }
-   pub async fn next(&mut self) -> Option<Warc> {
-        if(self.can_poll) {
+    pub async fn next(&mut self) -> Option<Warc> {
+        if self.can_poll {
             if let Some(chunk) = self.response.chunk().await.unwrap() {
-                self.add_to_buf(&chunk[..]);
-                self.attempt_drain(); 
+                self.attempt_drain(&chunk[..]);
             } else {
-               self.can_poll = false;
+                self.can_poll = false;
             }
         }
-        if (self.records.len() == 0 && self.can_poll == false)  {
+        if self.records.len() == 0 && self.can_poll == false {
             return None;
         } else {
             if self.records.len() == 0 {
@@ -38,19 +40,23 @@ impl WarcParser {
                 return Some(Warc::WarcResult(self.records.remove(0)));
             }
         }
-   }
-    pub fn add_to_buf(&mut self, chunk: &[u8]) {
-        let v = chunk;
-        self.buffers = [&self.buffers[..], &v[..]].concat();
     }
-    pub fn attempt_drain(&mut self) {
-        let buf = &self.buffers[..];
-        let mut warc = WarcReader::new(buf);
+    pub fn attempt_drain(&mut self, chunk: &[u8]) {
+        self.buffers = [&self.buffers[..], &chunk[..]].concat();
+        let mut warc = WarcReader::new(&self.buffers[..]);
         loop {
             let item: Option<Result<WarcRecord, WarcError>> = warc.next();
             match item {
                 Some(Ok(record)) => {
-                    self.records.push(record);
+                    if let Some(id) = record.header.get(&"warc-record-id".into()) {
+                        let new_id = id.clone();
+                        if !self.record_ids.contains_key(id) {
+                            if record.header.get(&"warc-target-uri".into()).is_some() {
+                                self.records.push(record);
+                                self.record_ids.insert(new_id.to_string(), true);
+                            }
+                        }
+                    }
                 }
                 Some(Err(_e)) => {
                     self.buffers = self.buffers[warc.sum..].to_vec();
